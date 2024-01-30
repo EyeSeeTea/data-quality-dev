35de2687fed2cd6334a930f249b67ee2af63fa43
mariaozamiz <ozamiz.m@gmail.com>
2024-01-30 11:06:03 +0100

    chore: add translations

--- src/webapp/pages/analysis/steps.ts
+++ src/webapp/pages/analysis/steps.ts
@@ -1,3 +1,4 @@
+import i18n from "../../../utils/i18n";
 import { ConfigurationStep } from "./steps/ConfigurationStep";
 import { DensityStep } from "./steps/DensityStep";
 import { DisaggregatesStep } from "./steps/DisaggregatesStep";
@@ -12,52 +13,52 @@ import { TrendsStep } from "./steps/TrendsStep";
 export const steps = [
     {
         key: "configuration",
-        label: "Configuration",
+        label: i18n.t("Configuration"),
         component: ConfigurationStep,
     },
     {
         key: "outliers",
-        label: "Outliers",
+        label: i18n.t("Outliers"),
         component: OutliersStep,
     },
     {
         key: "trends",
-        label: "Trends",
+        label: i18n.t("Trends"),
         component: TrendsStep,
     },
     {
         key: "disaggregates",
-        label: "Disaggregates",
+        label: i18n.t("Disaggregates"),
         component: DisaggregatesStep,
     },
     {
         key: "doctors",
-        label: "Doctors",
+        label: i18n.t("Doctors"),
         component: DoctorsStep,
     },
     {
         key: "nursing",
-        label: "Nursing",
+        label: i18n.t("Nursing"),
         component: NursingStep,
     },
     {
         key: "midwifery",
-        label: "Midwifery",
+        label: i18n.t("Midwifery"),
         component: MidwiferyStep,
     },
     {
         key: "nursing-midwifery",
-        label: "Nursing/Midwifery",
+        label: i18n.t("Nursing/Midwifery"),
         component: NursingMidwiferyStep,
     },
     {
         key: "density",
-        label: "Density",
+        label: i18n.t("Density"),
         component: DensityStep,
     },
     {
         key: "other",
-        label: "Other",
+        label: i18n.t("Other"),
         component: OtherStep,
     },
 ];
