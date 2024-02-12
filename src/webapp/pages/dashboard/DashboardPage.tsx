7ee92537808b8d18a9dfbd44e0ff14f9d13e013b
mariaozamiz <ozamiz.m@gmail.com>
2024-02-12 16:03:55 +0100

    feat: add filter with mocked data

--- src/webapp/pages/dashboard/DashboardPage.tsx
+++ src/webapp/pages/dashboard/DashboardPage.tsx
@@ -3,17 +3,26 @@ import i18n from "$/utils/i18n";
 import { PageHeader } from "$/webapp/components/page-header/PageHeader";
 import { ObjectsTable } from "@eyeseetea/d2-ui-components";
 import { columns, mockRows } from "./mock";
+import { useDashboard } from "./hooks/useDashboard";
 
 type Props = {
     name: string;
 };
 
 export const DashboardPage: React.FC<Props> = React.memo(props => {
+    const { customFilters, handleSearchChange } = useDashboard();
     const { name } = props;
     return (
         <>
             <PageHeader title={i18n.t(name)} />
-            <ObjectsTable rows={mockRows} columns={columns} allowReorderingColumns={false} />
+            <ObjectsTable
+                rows={mockRows}
+                columns={columns}
+                allowReorderingColumns={false}
+                filterComponents={customFilters}
+                searchBoxLabel={i18n.t("Name")}
+                onChangeSearch={handleSearchChange}
+            />
         </>
     );
 });
