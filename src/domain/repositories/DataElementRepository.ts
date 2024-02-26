4fa0984fde37787ce5533d5bf95e9227622eeed3
Eduardo Peredo Rivero <jrperedo@gmail.com>
2024-02-26 06:24:51 -0500

    delete unused repositories and implementations

--- src/domain/repositories/DataElementRepository.ts
+++
@@ -1,7 +0,0 @@
-import { FutureData } from "../../data/api-futures";
-import { DataElement } from "../entities/DataElement";
-import { Id } from "../entities/Ref";
-
-export interface DataElementRepository {
-    getByIds(ids: Id[]): FutureData<DataElement[]>;
-}
