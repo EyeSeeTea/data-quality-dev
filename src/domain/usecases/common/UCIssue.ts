6420bb77edd434a7cc03b7aa873c87f948d8884a
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-08-05 12:12:41 +0800

    rename merge for clarity

--- src/domain/usecases/common/UCIssue.ts
+++ src/domain/usecases/common/UCIssue.ts
@@ -171,7 +171,7 @@ export class UCIssue {
                 existingIssues: existingIssues,
                 newIssues: issues,
             });
-            return dimissedAnalysis.mergeDuplicates();
+            return dimissedAnalysis.inheritDismissedDuplicates();
         });
     }
 }
